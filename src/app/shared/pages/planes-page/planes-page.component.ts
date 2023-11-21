16aeba89e6d2d350624e5ea1e80100d11e66747e
Alberto Gómez <alberto.grodriguez@bosonit.com>
2023-11-21 12:54:42 +0100

    Pagina de tarifas

--- src/app/shared/pages/planes-page/planes-page.component.ts
+++ src/app/shared/pages/planes-page/planes-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-planes-page',
   templateUrl: './planes-page.component.html',
-  styleUrls: ['./planes-page.component.css']
+  styleUrls: ['./planes-page.component.css'],
 })
 export class PlanesPageComponent {
+  isHovered: boolean = false;
 
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 }
